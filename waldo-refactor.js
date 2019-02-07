5f9ed4a2a5d8d03707e3d7835d44aa9579747d1f
Matt Cook <mcc1985@gmail.com>
2019-02-07 14:31:21 +0000

    add orig waldo to refactor file, commented

--- waldo-refactor.js
+++ waldo-refactor.js
@@ -14,3 +14,13 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
