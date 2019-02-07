4ef5fe3922f3d452bd6b89e59863e7f64d6b2b5e
Matt Cook <mcc1985@gmail.com>
2019-02-07 20:09:26 +0000

    add countdown closure assign
    add loaded die closure assign
    
    logging-wrapper assign initial add

---
+++ loaded-die.js
@@ -0,0 +1,39 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var i = -1;
+
+  return function() {
+    if (i === list.length-1) {
+      i = -1;
+    }
+    i++;
+    return list[i];
+  };
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
